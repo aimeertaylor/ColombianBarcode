f3430c7534c2baeb4e48d7a4cbb7dce3424530e8
Aimee R. Taylor <aimee.r.taylor.85@gmail.com>
2019-03-19 13:31:18 -0400

    Adding gravity model script

---
+++ Rscripts/Colombia_gravity_model.R
@@ -0,0 +1,68 @@
+###########################################################
+# Colombia gravity model
+# Last census in 2005 
+# Tutorial following:
+# http://www.guilles.website/2017/12/16/exploring-worldpop-gis-data-with-r/
+#
+# To-do
+# Choose year
+# Finish tutorial
+###########################################################
+
+library(rgdal)
+library(raster)
+library(sp)
+library(colorRamps)
+library(ggmap)
+library(ggplot2)
+PLOT = F
+
+# Load Colombia data 
+WorldPopCol <- raster("~/Documents/Colombia_data/Colombia_100m_Population/COL_pph_v2b_2010.tif")
+
+# Shape file
+gtmMunis = readOGR("~/Documents/Colombia_data/COL_adm/COL_adm2.shp", encoding = "utf-8")
+
+# Plot world pop data  (takes a few seconds)
+if(PLOT){
+  plot(log10(WorldPopCol), colNA="black", 
+       col=colorRampPalette(c("#121212", "#123620", "#108650", "#80D6C0", "#DFDF3B"),0.5)(110))
+}
+ 
+# Convert adm2 name to character
+gtmMunis$NAME_2 = as.character(gtmMunis$NAME_2)
+
+# Look for city names
+gtmMunis$NAME_2[grepl('Guap', gtmMunis$NAME_2)] # "Guapí"
+gtmMunis$NAME_2[grepl('Tumac', gtmMunis$NAME_2)] # "Tumaco"
+gtmMunis$NAME_2[grepl('Buenaventura', gtmMunis$NAME_2)] # "Buenaventura"
+gtmMunis$NAME_2[grepl('Quibd', gtmMunis$NAME_2)] # "Quibdó"
+gtmMunis$NAME_2[grepl('Tad', gtmMunis$NAME_2)] # "Tadó"
+
+# City names
+sites = c("Guapí", "Tumaco", "Buenaventura", "Quibdó", "Tadó")
+
+# Population per site  
+populations = sapply(sites, function(site){
+  sum(extract(WorldPopCol, gtmMunis[gtmMunis$NAME_2==site,], fun = sum, na.rm = TRUE))
+})
+
+# Name without accents
+names(populations) = c("Guapi", "Tumaco", "Buenaventura", "Quibdo", "Tado")
+# save(populations, file = '~/Documents/Colombia_data/populations.RData')
+
+# Load distances
+load('../RData/populations.RData')
+load('../RData/geo_dist_info.RData')
+
+geo_dist_info$pairwise_site_distance$gravity_estimate = NA
+for(i in 1:nrow(geo_dist_info$pairwise_site_distance)){
+  x1 = as.character(geo_dist_info$pairwise_site_distance[i,'X1'])
+  x2 = as.character(geo_dist_info$pairwise_site_distance[i,'X2'])
+  g = populations[x1] * populations[x2] / (geo_dist_info$pairwise_site_distance[i,'distance'])^2
+  geo_dist_info$pairwise_site_distance[i, 'gravity_estimate'] = g
+}
+geo_dist_info$pairwise_site_distance
+
+
+  

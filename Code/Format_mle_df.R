9d98a8a915258e98fd850ea939667311f6c4d715
artaylor85 <aimee.r.taylor.85@gmail.com>
2019-04-27 18:21:44 -0400

    Edits to accommodate misspecified frequencies

--- Code/Format_mle_df.R
+++ Code/Format_mle_df.R
@@ -7,64 +7,61 @@
 # Adapt this script
 ##############################################
 rm(list = ls())
-load('../RData/mles.RData') # Few mins
-mle_core = mle_df[,1:4] # Extract core
 
-#---------------------------------------------
-# Save a version withs rhat CIs 
-#---------------------------------------------
-P = c(0.025, 0.975) # CI quantiles
-rhat_CIs = t(apply(mle_df, 1, function(x)quantile(x$rhats_boot, probs=P))) # Few seconds
-mle_CIs = cbind(mle_core, rhat_CIs)
-
-# Do some formating 
-if(class(mle_CIs$individual1) == 'factor'){mle_CIs$individual1 = as.character(mle_CIs$individual1)}
-if(class(mle_CIs$individual2) == 'factor'){mle_CIs$individual2 = as.character(mle_CIs$individual2)}
-
-# Load data to add meta data 
-load('../RData/SNPData.RData') 
-load('../RData/geo_dist_info_cities.RData')
-geo_dist_info_cities = geo_dist_info
-load('../RData/geo_dist_info_states.RData')
-geo_dist_info_states = geo_dist_info
-
-# Add city comps
-mle_CIs$City1 = as.character(SNPData[mle_CIs$individual1, 'City'])
-mle_CIs$City2 = as.character(SNPData[mle_CIs$individual2, 'City'])
-mle_CIs$City12 = apply(mle_CIs[,c('City1','City2')], 1, function(x)paste(sort(x),collapse="_"))
-
-mle_CIs$State1 = as.character(SNPData[mle_CIs$individual1, 'STATE'])
-mle_CIs$State2 = as.character(SNPData[mle_CIs$individual2, 'STATE'])
-mle_CIs$State12 = apply(mle_CIs[,c('State1','State2')], 1, function(x)paste(sort(x),collapse="_"))
-
-# Add ditance for cities
-mle_CIs$geo_dist_cities = geo_dist_info_cities$pairwise_site_distance_all[mle_CIs$City12]
-mle_CIs$geo_dist_states = geo_dist_info_states$pairwise_site_distance_all[mle_CIs$State12]
-
-# Add dates and distance in time 
-mle_CIs$date1 = as.character(SNPData[mle_CIs$individual1, 'COLLECTION.DATE'])
-mle_CIs$date2 = as.character(SNPData[mle_CIs$individual2, 'COLLECTION.DATE'])
-mle_CIs$time_dist = abs(difftime(mle_CIs$date1, mle_CIs$date2, units = 'weeks'))
-
-# Add distance in time bins
-interval <- 50 # The resolution of time bins in weeks
-max_time_dist = max(as.numeric(mle_CIs$time_dist))
-time_breaks <- seq(0, max_time_dist, interval) 
-max_time_breaks = max(time_breaks)
-if(max_time_breaks < max_time_dist){time_breaks = c(time_breaks, max_time_breaks + interval)}
-time_bins = time_breaks[-length(time_breaks)] # The time bins value are the min for each bin
-no_time_bins = length(time_bins)
-
-# Add time bins 
-mle_CIs$time_bins = NA
-for(i in 1:no_time_bins){
-  ind <- (as.numeric(mle_CIs$time_dist) >= time_breaks[i]) & (as.numeric(mle_CIs$time_dist) < time_breaks[i+1])
-  mle_CIs$time_bins[ind] = time_bins[i]
+for(f in c("true", "unif")){
+  
+  load(sprintf('../RData/mles_frequencies_%s.RData', f)) # Few mins
+  mle_CIs = mle_df # To remove
+  
+  # Do some formating 
+  if(class(mle_CIs$individual1) == 'factor'){mle_CIs$individual1 = as.character(mle_CIs$individual1)}
+  if(class(mle_CIs$individual2) == 'factor'){mle_CIs$individual2 = as.character(mle_CIs$individual2)}
+  
+  # Load data to add meta data 
+  load('../RData/SNPData.RData') 
+  load('../RData/geo_dist_info_cities.RData')
+  geo_dist_info_cities = geo_dist_info
+  load('../RData/geo_dist_info_states.RData')
+  geo_dist_info_states = geo_dist_info
+  
+  # Add city comps
+  mle_CIs$City1 = as.character(SNPData[mle_CIs$individual1, 'City'])
+  mle_CIs$City2 = as.character(SNPData[mle_CIs$individual2, 'City'])
+  mle_CIs$City12 = apply(mle_CIs[,c('City1','City2')], 1, function(x)paste(sort(x),collapse="_"))
+  
+  mle_CIs$State1 = as.character(SNPData[mle_CIs$individual1, 'STATE'])
+  mle_CIs$State2 = as.character(SNPData[mle_CIs$individual2, 'STATE'])
+  mle_CIs$State12 = apply(mle_CIs[,c('State1','State2')], 1, function(x)paste(sort(x),collapse="_"))
+  
+  # Add ditance for cities
+  mle_CIs$geo_dist_cities = geo_dist_info_cities$pairwise_site_distance_all[mle_CIs$City12]
+  mle_CIs$geo_dist_states = geo_dist_info_states$pairwise_site_distance_all[mle_CIs$State12]
+  
+  # Add dates and distance in time 
+  mle_CIs$date1 = as.character(SNPData[mle_CIs$individual1, 'COLLECTION.DATE'])
+  mle_CIs$date2 = as.character(SNPData[mle_CIs$individual2, 'COLLECTION.DATE'])
+  mle_CIs$time_dist = abs(difftime(mle_CIs$date1, mle_CIs$date2, units = 'weeks'))
+  
+  # Add distance in time bins
+  interval <- 50 # The resolution of time bins in weeks
+  max_time_dist = max(as.numeric(mle_CIs$time_dist))
+  time_breaks <- seq(0, max_time_dist, interval) 
+  max_time_breaks = max(time_breaks)
+  if(max_time_breaks < max_time_dist){time_breaks = c(time_breaks, max_time_breaks + interval)}
+  time_bins = time_breaks[-length(time_breaks)] # The time bins value are the min for each bin
+  no_time_bins = length(time_bins)
+  
+  # Add time bins 
+  mle_CIs$time_bins = NA
+  for(i in 1:no_time_bins){
+    ind <- (as.numeric(mle_CIs$time_dist) >= time_breaks[i]) & (as.numeric(mle_CIs$time_dist) < time_breaks[i+1])
+    mle_CIs$time_bins[ind] = time_bins[i]
+  }
+  
+  # Add sample comp
+  mle_CIs$sample_comp = apply(mle_CIs[, c("individual1", "individual2")], 1, 
+                              function(x)paste(sort(x), collapse = "_"))
+  
+  # Save data frame
+  save(mle_CIs, file = sprintf("../RData/mles_%s.RData", f))
 }
-
-# Add sample comp
-mle_CIs$sample_comp = apply(mle_CIs[, c("individual1", "individual2")], 1, 
-                            function(x)paste(sort(x), collapse = "_"))
-
-# Save data frame
-save(mle_CIs, file = '../RData/mle_CIs.RData')
--- Code/Generate_mles_CIs.R
+++ Code/Generate_mles_CIs.R
@@ -16,8 +16,9 @@ source("~/Dropbox/IBD_IBS/PlasmodiumRelatedness/Code/simulate_data.R") # Downloa
 sourceCpp("~/Dropbox/IBD_IBS/PlasmodiumRelatedness/Code/hmmloglikelihood.cpp") # Download this script from https://github.com/artaylor85/PlasmodiumRelatedness
 registerDoParallel(cores = detectCores()-1)
 epsilon <- 0.001 # Fix epsilon throughout
-nboot <- 500 # For CIs 
+nboot <- 2 # For CIs 
 set.seed(1) # For reproducibility
+Ps = c(0.025, 0.975) # CI quantiles
 
 ## Mechanism to compute MLE given fs, distances, Ys, epsilon
 compute_rhat_hmm <- function(frequencies, distances, Ys, epsilon){
@@ -53,42 +54,51 @@ for (i in 1:(nindividuals-1)){
 # Sort data by chromosome and position and add frequencies
 data_set <- data_set %>% arrange(chrom, pos) 
 data_set$fs = rowMeans(data_set[,-(1:2)], na.rm = TRUE) # Calculate frequencies
-frequencies <- cbind(1-data_set$fs, data_set$fs)
 data_set$dt <- c(diff(data_set$pos), Inf)
 pos_change_chrom <- 1 + which(diff(data_set$chrom) != 0) # find places where chromosome changes
 data_set$dt[pos_change_chrom-1] <- Inf
 
-system.time(
-# For each pair...  
-mle_df <- foreach(icombination = 1:nrow(name_combinations),.combine = rbind) %dorng% {
-  
-  # Let's focus on one pair of individuals
-  individual1 <- name_combinations[icombination,1]
-  individual2 <- name_combinations[icombination,2]
-  
-  # Indeces of pair
-  i1 <- which(individual1 == names(data_set))
-  i2 <- which(individual2 == names(data_set))
-  
-  # Extract data 
-  subdata <- cbind(data_set[,c("fs","dt")],data_set[,c(i1,i2)])
-  names(subdata) <- c("fs","dt","Yi","Yj")
-  
-  # Generate mle
-  krhat_hmm <- compute_rhat_hmm(frequencies, subdata$dt, cbind(subdata$Yi, subdata$Yj), epsilon)
-  
-  # Generate parametric bootstrap mles 
-  krhats_hmm_boot = foreach(iboot = 1:nboot, .combine = rbind) %dorng% {
-    Ys_boot <- simulate_Ys_hmm(frequencies, distances = data_set$dt, k = krhat_hmm[1], r = krhat_hmm[2], epsilon)
-    compute_rhat_hmm(frequencies, subdata$dt, Ys_boot, epsilon)
-  }
-  
-  X = data.frame(individual1 = individual1, individual2 = individual2, khat = krhat_hmm[1], rhat = krhat_hmm[2])
-  X$khats_boot = list(krhats_hmm_boot[,1])
-  X$rhats_boot = list(krhats_hmm_boot[,2])
-  X
-})
 
-# CIs <- apply(krhats_hmm_boot, 2, quantile, probs = c(0.025, 0.975))
-#save(mle_df, file = paste0("../RData/mles.RData"))
+funif = runif(250)
+frequencies_unif <- cbind(funif, 1-funif) 
+frequencies_true <- cbind(1-data_set$fs, data_set$fs)
+f_list = list(frequencies_true = frequencies_true, frequencies_unif = frequencies_unif)
+
+for(f in 1:2){
+  
+  frequencies = f_list[[f]]
+  system.time(
+    
+    # For each pair...  
+    mle_CIs <- foreach(icombination = 1:nrow(name_combinations),.combine = rbind) %dorng% {
+      
+      # Let's focus on one pair of individuals
+      individual1 <- name_combinations[icombination,1]
+      individual2 <- name_combinations[icombination,2]
+      
+      # Indeces of pair
+      i1 <- which(individual1 == names(data_set))
+      i2 <- which(individual2 == names(data_set))
+      
+      # Extract data 
+      subdata <- cbind(data_set[,c("fs","dt")],data_set[,c(i1,i2)])
+      names(subdata) <- c("fs","dt","Yi","Yj")
+      
+      # Generate mle
+      krhat_hmm <- compute_rhat_hmm(frequencies, subdata$dt, cbind(subdata$Yi, subdata$Yj), epsilon)
+      
+      # Generate parametric bootstrap mles 
+      krhats_hmm_boot = foreach(iboot = 1:nboot, .combine = rbind) %dorng% {
+        Ys_boot <- simulate_Ys_hmm(frequencies, distances = data_set$dt, k = krhat_hmm[1], r = krhat_hmm[2], epsilon)
+        compute_rhat_hmm(frequencies, subdata$dt, Ys_boot, epsilon)
+      }
+      
+      CIs = apply(krhats_hmm_boot, 2, function(x)quantile(x, probs=Ps)) # Few seconds
+      X = data.frame('individual1' = individual1, 'individual2' = individual2, 
+                     rhat = krhat_hmm[2], 'r2.5%' = CIs[1,2], 'r97.5%' = CIs[2,2],
+                     khat = krhat_hmm[1], 'k2.5%' = CIs[1,1], 'k97.5%' = CIs[2,1])
+      X
+    })
+  save(mle_CIs, file = sprintf("../RData/mles_%s.RData", names(f_list)[f]))
+}
 

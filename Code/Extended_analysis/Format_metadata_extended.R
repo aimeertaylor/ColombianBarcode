d9b7bc0602fe9dee215e9772eb9a33b5a3d9b914
artaylor85 <aimee.r.taylor.85@gmail.com>
2021-01-22 14:02:51 +0100

    Fix missing extended

--- Code/Extended_analysis/Format_metadata_extended.R
+++ Code/Extended_analysis/Format_metadata_extended.R
@@ -147,7 +147,7 @@ lapply(metadata_extended, function(x) unique(x))
 head(metadata_extended)
 
 # Add a column to label data origin
-metadata$PloSGen2020 <- metadata$SAMPLE.CODE %in% SNPData$SAMPLE.CODE
+metadata_extended$PloSGen2020 <- metadata$SAMPLE.CODE %in% SNPData$SAMPLE.CODE
 
 # =============== Save the extended set of metadata  ===============
 metadata <- metadata_extended

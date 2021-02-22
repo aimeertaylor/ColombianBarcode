1e35799e32d381539c713c20aecd2f6488da226e
artaylor85 <aimee.r.taylor.85@gmail.com>
2021-02-22 11:54:48 +0100

    Spring clean

--- Code/Extended_analysis/Checking_mles.R
+++ Code/Extended_analysis/Checking_mles.R
@@ -1,6 +1,13 @@
+################################################################################
+#' This script compares original and extended estimates and CIs for DE samples.
+#' There are a few likely inconsequential differences. We then looks for CI
+#' limit inconsistencies, of which there are a few on the LCI, due to underflow
+#' problems
+################################################################################
 rm(list = ls())
 
-# Original mles (NAs were not considered in CIs)
+# Original mles (NAs were not considered in CIs
+# and there was a very minor NA problem in hmmloglikelihood.cpp)
 load('../../RData/mles_CIs.RData')
 mles_original <- mle_CIs
 
@@ -25,18 +32,23 @@ rownames(mles_extended) <- paste(mles_extended$individual1,
 # Check all there
 all(rownames(mles_original) %in% rownames(mles_extended))
 
-# Compare estimates: yes, match. 
+# Compare estimates: not all. 
 plot(x = mles_original$rhat, 
      y = mles_extended[rownames(mles_original), "rhat"])
 
-# Compare CIs: no, not exact match (random numbers different)
+# Are mismatches due to snp counts < 250? No
+mismatch_ind <- which(abs(mles_original$rhat- mles_extended[rownames(mles_original), "rhat"]) > 0.01)
+plot(y = abs(mles_original$rhat - mles_extended[rownames(mles_original), "rhat"]), 
+     x = mles_extended[rownames(mles_original), "snp_count"], 
+     xlab = "Shared SNP count", ylab = "Difference in estimates")
+
+# Compare CIs: don't expect exact match (random numbers different, NA treatment different)
 plot(x = mles_original$r97.5., 
      y = mles_extended[rownames(mles_original), "r97.5."])
 
 # Compare exactly 0.5 
 any(mles_original$rhat == 0.5) # No 
-# all snp_count <= 5 and uninformative
-mles_extended[which(mles_extended$rhat == 0.5),] 
+any(mles_extended[rownames(mles_original), "rhat"] == 0.5) # No 
 
 # Compare uninformative
 eps <- 0.01
@@ -56,14 +68,13 @@ clones_extended_only <- setdiff(clones_extended, clones_original)
 clones_original_only <- setdiff(clones_original, clones_extended)
 c(clones_extended_only, clones_original_only) # Only four differences
 
-# Compare CI widths (expect extended to be wider but not)
+# Compare CI widths (expect extended to be wider but only slightly)
 mles_original$CI_width <- mles_original$r97.5. - mles_original$r2.5.
 mles_extended$CI_width <- mles_extended$r97.5. - mles_extended$r2.5.
 summary(mles_original$CI_width)
 summary(mles_extended[rownames(mles_original), "CI_width"])
 plot(x = mles_original$CI_width, 
      y = mles_extended[rownames(mles_original), "CI_width"])
-
 
 #===== Check for inconsistent CI limits =====#
 
@@ -77,12 +88,11 @@ sum(ind_rhat_bigg_thn_UCI_original)
 mles_rhat_less_thn_LCI_original <- mles_original[ind_rhat_less_thn_LCI_original, ]
 unique(mles_rhat_less_thn_LCI_original[,c("r2.5.", "rhat")])
 
-# Check its really an under/overflow problem by rounding: yes
+# Check go away when round
 UCI <- round(mles_original$r97.5., 5)
 LCI <- round(mles_original$r2.5., 5)
 est <- round(mles_original$rhat, 5)
 mles_original[which((LCI > est) | (UCI < est)), ]
-
 
 # Across all matched extended estimates: also seems to be an underflow issue
 ind_rhat_less_thn_LCI_extended <- mles_extended[rownames(mles_original), 'rhat'] < 
@@ -96,34 +106,20 @@ sum(ind_rhat_bigg_thn_UCI_extended, na.rm = T)
 mles_rhat_less_thn_LCI_extended <- mles_extended[rownames(mles_original),][ind_rhat_less_thn_LCI_extended, ]
 unique(mles_rhat_less_thn_LCI_extended[,c("r2.5.", "rhat")])
 
-
-# Across all matched extended estimates: also seems to be an underflow issue
+# Across all extended estimates: also seems to be an underflow issue
 ind_rhat_less_thn_LCI_extended <- mles_extended$rhat < mles_extended$r2.5.
 ind_rhat_bigg_thn_UCI_extended <- mles_extended$rhat > mles_extended$r97.5.
-# problem on both ends
+# problem on LCI only
 sum(ind_rhat_less_thn_LCI_extended, na.rm = T)
 sum(ind_rhat_bigg_thn_UCI_extended, na.rm = T)
 # Problem on inspection appears to be under/over flow for rhat < r2.5.
 mles_rhat_less_thn_LCI_extended <- mles_extended[ind_rhat_less_thn_LCI_extended, ]
 unique(mles_rhat_less_thn_LCI_extended[,c("rhat","r2.5.")])
-# Problem on inspection appears to be under/over flow for rhat > r97.5.
-mles_rhat_bigg_thn_UCI_extended <- mles_extended[ind_rhat_bigg_thn_UCI_extended, ]
-unique(mles_rhat_bigg_thn_UCI_extended[,c("rhat","r97.5.")])
 
-# Is it always associated with a low snpcount? No!
-sort(unique(mles_rhat_bigg_thn_UCI_extended$snp_count))
-example_pairs <- rownames(unique(mles_rhat_bigg_thn_UCI_extended[,c("rhat","r97.5.")]))
-mles_rhat_bigg_thn_UCI_extended[example_pairs, c("rhat","r97.5.","snp_count")]
-
-# Check its really an under/overflow problem by rounding: yes, 
+# Check it goes away with rounding: yes, 
 # goes away if we round to two decimal places
 # can check by manually changing the precision to which we round 
 UCI <- round(mles_extended$r97.5., 2) 
 LCI <- round(mles_extended$r2.5., 2)
 est <- round(mles_extended$rhat, 2)
 mles_extended[which((LCI > est) | (UCI < est)), ]
-
-# Example problems: any of example_pairs
-# (check by hand computation returned by Generate_mles_CIs_extended):
-which(rownames(mles_extended) == "PW0082-C_Pf007") # 124402 # snp_count = 8
-which(rownames(mles_extended) == "PW0104-C_SPT26297") # 127456 # snp_count = 165!

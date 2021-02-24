b636005e2acb8d8d84017214a8b0acebb9a7909b
artaylor85 <aimee.r.taylor.85@gmail.com>
2021-02-24 18:18:16 +0100

    Modify so only samples missing one or more relatedness estimates

--- Code/Extended_analysis/Generate_sids_remv.R
+++ Code/Extended_analysis/Generate_sids_remv.R
@@ -1,11 +1,11 @@
 ##############################################################################
-#' This script was written to find, among FSVC samples, those to filter before
-#' generating graphs. We do this by computing the number of NA relatedness
-#' estimates per sample, removing the sample with the highest count and
-#' iterating. NB, we allow any CI width and see what CI widths we're left with
-#' after removing samples. NB, although there is a clear association between
-#' per-sample NA relatedness counts and per-sample marker data count, the
-#' removed sample doesn't always have the minimum snp count.
+#' This script was written to find samples to filter before generating graphs.
+#' We do this by computing the number of NA relatedness estimates per sample,
+#' removing the sample with the highest count and iterating. NB, we allow any CI
+#' width and see what CI widths we're left with after removing samples. NB,
+#' although there is a clear association between per-sample NA relatedness
+#' counts and per-sample marker data count, the removed sample doesn't always
+#' have the minimum snp count.
 #'
 #' Could we use the transitivity property of clones etc. to impute NAs? This
 #' third point is a project in itself.
@@ -18,12 +18,8 @@ source('../igraph_functions.R') # construct_adj_matrix
 load('../../RData/mles_CIs_extended_freqsTaylor2020_meta.RData') 
 load('../../RData/metadata_extended.RData')
 
-# Extract sids ordered by date
-FSVC_sid <- dplyr::arrange(metadata[!metadata$PloSGen2020, ], COLLECTION.DATE)$SAMPLE.CODE
-FSVC_pair_ind <- (mle_CIs$individual1 %in% FSVC_sid) & (mle_CIs$individual2 %in% FSVC_sid)
-
 # Extract estimates into an upper adjacency matrix and make symmetric
-A_est <- construct_adj_matrix(mle_CIs[FSVC_pair_ind, ], Entry = 'rhat')
+A_est <- construct_adj_matrix(mle_CIs, Entry = 'rhat')
 A_est_full <- A_est
 A_est_full[lower.tri(A_est_full)] <- t(A_est_full)[lower.tri(A_est_full)]
 diag(A_est_full) <- 1
@@ -75,30 +71,19 @@ for(i in 1:length(A_est_fulls)){
 }
 
 # Summarise samples kept and removed
-sids_keep <- unique(unlist(dimnames(A_est_full_new)))
-
-# Samples excluded in Filter_mles_CIs_extended.R: 
-setdiff(FSVC_sid, c(mle_CIs$individual1[FSVC_pair_ind], mle_CIs$individual2[FSVC_pair_ind]))
-
-# Samples removed here only: 
-setdiff(c(mle_CIs$individual1[FSVC_pair_ind], mle_CIs$individual2[FSVC_pair_ind]), sids_keep)
+sids_keep <- unique(colnames(A_est_full_new))
 
 # Samples removed in Filter_mles_CIs_extended.R and here: 
-sids_remv <- setdiff(FSVC_sid, sids_keep)
+sids_remv <- setdiff(unique(colnames(A_est)), sids_keep)
 
-# Metadata of removed SNPs
-table(metadata[FSVC_sid, "City"])
-table(metadata[sids_keep, "City"])
-table(metadata[sids_remv, "City"])
-
-# If we use this threshold, do we remove all the uninformative estimates? No
+# When we remove, do we remove all the uninformative estimates? No
 inds <- mle_CIs$individual1 %in% sids_keep & mle_CIs$individual2 %in% sids_keep
 range(mle_CIs$CI_width[inds])
 
 save(sids_remv, file = "../../RData/sids_to_remove_from_graphs.RData")
 
 source("../../Code/Extended_analysis/summarise_mles.R")
-summarise_mles(mle_CIs[FSVC_pair_ind,], metadata_ = metadata)
+summarise_mles(mle_CIs, metadata_ = metadata)
 summarise_mles(mle_CIs[inds, ], metadata_ = metadata)
 
 

35ef48006800e92e4402795e92c42b562f3c68c3
artaylor85 <aimee.r.taylor.85@gmail.com>
2021-03-04 14:41:25 +0100

    Update for new dataset

--- Code/Extended_analysis/Format_snpdata_extended.R
+++ Code/Extended_analysis/Format_snpdata_extended.R
@@ -5,7 +5,7 @@ rm(list = ls())
 
 # =============== Load data provided by Angela ===============
 # email sent 21st Dec 2020 in thread entitled "Golden Gate Recoded Data"
-dataset <- read.delim("../../recodedgoldengatedata/Diego-Vladimir-Fabian_GG3D7_Recode_21Dec2020.txt", 
+dataset <- read.delim("../../OriginalData/GGCalls_based_on_3d7_w_Guapi_for_Aimee_Mar21.txt", 
                       check.names = F, # Stops Xs being added to names beginning with a digit and conversion of "-" to "."
                       stringsAsFactors = F) 
 head(dataset[,1:10]) 
@@ -27,7 +27,7 @@ par(mfrow = c(1,1), family = 'serif', mar = c(2,2,1,1))
 cols <- RColorBrewer::brewer.pal(3,'Dark2')
 image(as.matrix(snpdata[,-(1:2)]), ylab = '', xlab = '', xaxt = 'n', yaxt = 'n', col = cols)
 
-which(rowSums(snpdata[,-(1:2)], na.rm = T) == 0) # Any markers with zero data: no
+which(rowSums(snpdata[,-(1:2)], na.rm = T) == 0) # Any markers with zero data: yes, one
 which(colSums(snpdata, na.rm = T) == 0) # Any samples with zero data: yes
 
 # =============== Save the extended data_set  ===============

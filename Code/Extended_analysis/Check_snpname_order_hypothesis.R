8f982901ba4be6de197ac8a9d0c95f893eea70da
artaylor85 <aimee.r.taylor.85@gmail.com>
2021-03-15 12:57:15 +0100

    Script that confirms hypothesis that marker names miss-ordered alphabetically in ColombianDataset_from_DiegoEcheverry_with_cities.csv

---
+++ Code/Extended_analysis/Check_snpname_order_hypothesis.R
@@ -0,0 +1,34 @@
+########################################################################
+# Script written to test the hypothesis that the markers were ordered
+# alphabetically
+########################################################################
+require(dplyr)
+
+# Angela's mapping based on sequences across sid matching
+fuzzy_match <- read.delim("../../TxtData/GGCall_Version_Marker_Matches_RawCalls.txt")
+gsub(":Aimee","",fuzzy_match$Version1)
+
+# Mapping based on numerically reorderin
+snp_original_order <- read.csv("../../OriginalData/Colombian snp data and flanking secs.csv")[-1,]
+snp_reordered <- data.frame(snp_no = snp_original_order$X.11[1:250], 
+                            snp_name_original = snp_original_order$X.12[1:250])
+snp_reordered$snp_name_pos <- sapply(snp_original_order$X.12[1:250], function(x) as.numeric(strsplit(x, split = "-")[[1]][2]))
+snp_reordered$snp_name_chrom <- sapply(snp_original_order$X.12[1:250], function(x) as.numeric(gsub("MAL", "", strsplit(x, split = "-")[[1]][1])))
+snp_reordered$snp_name_reordered <- snp_reordered %>% 
+  arrange(snp_name_chrom, snp_name_pos) %>% 
+  pull(snp_name_original)
+
+
+pair_sids_recordered <- apply(snp_reordered[, c("snp_name_original", "snp_name_reordered")], 1, function(x) paste(sort(x), collapse = " "))
+pair_sids_fuzzymatch <- apply(cbind(gsub(":Aimee","",fuzzy_match$Version1), 
+                                    gsub(":Angela","",fuzzy_match$Version2)), 1, function(x) paste(sort(x), collapse = " "))
+
+
+test_result <- setequal(pair_sids_recordered, pair_sids_fuzzymatch)
+if(test_result) {
+  writeLines("Is alphabetically miss-ordered hypothesis correct? Yes")
+} else {
+  writeLines("Is alphabetically miss-ordered hypothesis correct? No")
+}
+
+

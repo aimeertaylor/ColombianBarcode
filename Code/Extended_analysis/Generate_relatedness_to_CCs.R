81afd7e425d559483f7592a765fe82bc69ce58b5
artaylor85 <aimee.r.taylor.85@gmail.com>
2021-03-26 13:08:04 +0100

    Add within cc all relatedness computation

--- Code/Extended_analysis/Generate_relatedness_to_CCs.R
+++ Code/Extended_analysis/Generate_relatedness_to_CCs.R
@@ -3,9 +3,7 @@
 # "new" and "old" CCs (CCs of Taylor et al.)
 # 
 # Notes: 
-# - Takes a few moments to run
-# - Consider replacing new CCs (Clonal_components_extended_FSVC_LCIthrehold_0.75.RData)
-#   with WGS-based clusters
+# - Takes a while to run, especially for cc_all
 ##############################################################
 rm(list = ls())
 
@@ -17,15 +15,17 @@ any(is.na(mle_CIs$rhat)) # Check for NAs
 # Load Clonal components 
 load("../../RData/Clonal_components.RData")
 CC_original <- Clonal_components
+
 load("../../RData/Clonal_components_extended_FSVC_LCIthrehold_0.75.RData")
 cc_extended <- Clonal_components
+
+load("../../RData/Clonal_components_extended_all_LCIthrehold_0.75.RData")
+cc_all <- Clonal_components
 
 # Load metadata for sorting sids below
 load("../../RData/metadata_extended.RData")
 
-# Get IDs of samples that remain after removing 
-# overly precise estimates close to zero and one; see Format_mles_CIs_extended.R
-# NAs and uninformative (above)
+# Get FSVC sample IDs that feature in mle_CIs
 FSVC_sid_all <- metadata$SAMPLE.CODE[!metadata$PloSGen2020]
 FSVC_sid_ind <- FSVC_sid_all %in% c(mle_CIs$individual1, mle_CIs$individual2) 
 FSVC_sid <- FSVC_sid_all[FSVC_sid_ind]
@@ -83,9 +83,54 @@ cc_extended_to_cc_original <- lapply(cc_extended, function(cc_e){
 }) 
 
 
+#======================================================
+# Relatedness estimates between all the 
+# clonal components within the extended set of inc. singletons
+#======================================================
+within_cc_extended_inc_singletons <- lapply(names(cc_all), function(cc_i){
+  sapply(setdiff(names(cc_all), cc_i), function(cc_j){
+    inds <- mle_CIs$individual1 %in% cc_all[[cc_i]] & 
+      mle_CIs$individual2 %in% cc_all[[cc_j]] |
+      mle_CIs$individual1 %in% cc_all[[cc_j]] & 
+      mle_CIs$individual2 %in% cc_all[[cc_i]]
+    if(any(inds)){
+      c(av_rhat = mean(mle_CIs[inds,"rhat"], na.rm = T), 
+        av_r2.5 = mean(mle_CIs[inds,"r2.5."], na.rm = T), 
+        av_r97.5 = mean(mle_CIs[inds,"r97.5."], na.rm = T), 
+        mn_r2.5 = min(mle_CIs[inds,"r2.5."], na.rm = T), 
+        mx_r97.5 = max(mle_CIs[inds,"r97.5."], na.rm = T))
+    } else {
+      c(av_rhat = NA, av_r2.5 = NA, av_r97.5 = NA, mn_r2.5 = NA, mx_r97.5 = NA)
+    }
+  })
+}) 
+
+# Relatedness estimates between all the 
+# clonal components within the extended set of exc. singletons
+#======================================================
+cc_all_exc_singletons <- names(which(sapply(cc_all, length) > 1))
+within_cc_extended_exc_singletons <- lapply(cc_all_exc_singletons, function(cc_i){
+  sapply(setdiff(cc_all_exc_singletons, cc_i), function(cc_j){
+    inds <- mle_CIs$individual1 %in% cc_all[[cc_i]] & 
+      mle_CIs$individual2 %in% cc_all[[cc_j]] |
+      mle_CIs$individual1 %in% cc_all[[cc_j]] & 
+      mle_CIs$individual2 %in% cc_all[[cc_i]]
+    if(any(inds)){
+      c(av_rhat = mean(mle_CIs[inds,"rhat"], na.rm = T), 
+        av_r2.5 = mean(mle_CIs[inds,"r2.5."], na.rm = T), 
+        av_r97.5 = mean(mle_CIs[inds,"r97.5."], na.rm = T), 
+        mn_r2.5 = min(mle_CIs[inds,"r2.5."], na.rm = T), 
+        mx_r97.5 = max(mle_CIs[inds,"r97.5."], na.rm = T))
+    } else {
+      c(av_rhat = NA, av_r2.5 = NA, av_r97.5 = NA, mn_r2.5 = NA, mx_r97.5 = NA)
+    }
+  })
+}) 
+
+
 # Save results
 save(sid_extended_to_cc_original, cc_extended_to_cc_original, 
+     within_cc_extended_inc_singletons, within_cc_extended_exc_singletons, 
      file = "../../RData/relatedness_to_CCs.RData")
-
 
 

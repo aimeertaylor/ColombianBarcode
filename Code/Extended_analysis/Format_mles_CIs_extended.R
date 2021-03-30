a842bcc8e3f5c9890ec0ff9ca827e526848c53a9
artaylor85 <aimee.r.taylor.85@gmail.com>
2021-03-30 21:18:49 +0200

    Make names more intuitive

--- Code/Extended_analysis/Format_mles_CIs_extended.R
+++ Code/Extended_analysis/Format_mles_CIs_extended.R
@@ -16,6 +16,10 @@ load(sprintf("../../RData/mles_CIs_extended_freqs%s_filtered.RData", freqs_used)
 if(class(mle_CIs$individual1) == 'factor'){mle_CIs$individual1 = as.character(mle_CIs$individual1)}
 if(class(mle_CIs$individual2) == 'factor'){mle_CIs$individual2 = as.character(mle_CIs$individual2)}
 
+# Compute and add the fraction highly related using 0.25 (threshold used in main analyses of Taylor et al. 2020)
+high_relatedness_threshold <- 0.25
+mle_CIs$highly_related <- as.numeric(mle_CIs$r2.5. > high_relatedness_threshold)
+
 # Add cities
 # Check NA returned for missing sids: 
 is.na(metadata['agsdhfsdhf', 'City'])
@@ -27,14 +31,18 @@ mle_CIs$State2 = metadata[mle_CIs$individual2, 'STATE']
 # Add city comparisons
 mle_CIs$City12 = apply(mle_CIs[,c('City1','City2')], 1, function(x)paste(sort(x),collapse="_"))
 
-# Add city distance
+# Add city distance and access
 city_comb <- unique(mle_CIs[, c("City1", "City2", "City12")])
 intercity_dists <- sapply(1:nrow(city_comb), function(i){
   gcd.hf(A = as.matrix(LonLats[city_comb[i, "City1"], c('Longitude', 'Latitude')]), 
          B = as.matrix(LonLats[city_comb[i, "City2"], c('Longitude', 'Latitude')]))
 })
 names(intercity_dists) <- city_comb$City12
-mle_CIs$int_dist <- intercity_dists[mle_CIs$City12]
+mle_CIs$geo_dist <- intercity_dists[mle_CIs$City12]
+
+# Access
+mle_CIs$Coast <- LonLats[mle_CIs$City1, "CoastAccess"] == 1 & LonLats[mle_CIs$City2, "CoastAccess"] == 1
+mle_CIs$Port <- LonLats[mle_CIs$City1, "Port"] == 1 & LonLats[mle_CIs$City2, "Port"] == 1
 
 # Check NA returned for missing sids: yes
 metadata['agsdhfsdhf', 'COLLECTION.DATE']
@@ -54,7 +62,7 @@ mle_CIs$time_dist = abs(difftime(as.Date(mle_CIs$date1),
                                  as.Date(mle_CIs$date2), units = 'weeks'))
 
 # Add year dist based on yearly label (e.g. not round to less than, more than a year)
-mle_CIs$year_dist <- abs(as.numeric(mle_CIs$year1) - as.numeric(mle_CIs$year2))
+mle_CIs$year_diff <- abs(as.numeric(mle_CIs$year1) - as.numeric(mle_CIs$year2))
 
 # Add sample comp
 mle_CIs$sample_comp = apply(mle_CIs[, c("individual1", "individual2")], 1, 

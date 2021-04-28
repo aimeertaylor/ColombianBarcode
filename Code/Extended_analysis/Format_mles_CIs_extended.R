55378c55b5a5342474a218dc1f18eaa09c6082cf
artaylor85 <aimee.r.taylor.85@gmail.com>
2021-04-28 13:31:20 +0200

    Add country and modify Coast and Port name

--- Code/Extended_analysis/Format_mles_CIs_extended.R
+++ Code/Extended_analysis/Format_mles_CIs_extended.R
@@ -20,18 +20,30 @@ if(class(mle_CIs$individual2) == 'factor'){mle_CIs$individual2 = as.character(ml
 high_relatedness_threshold <- 0.25
 mle_CIs$highly_related <- as.numeric(mle_CIs$r2.5. > high_relatedness_threshold)
 
+# Add State and country, noting that metadata STATE contains a mix of both
+mle_CIs$State1 = metadata[mle_CIs$individual1, 'STATE']
+mle_CIs$State2 = metadata[mle_CIs$individual2, 'STATE']
+mle_CIs$Country1 = mle_CIs$State1
+mle_CIs$Country2 = mle_CIs$State2
+ind_EV1 <- (mle_CIs$Country1 == "Ecuador" | mle_CIs$Country1 == "Venezuela")
+ind_EV2 <- (mle_CIs$Country2 == "Ecuador" | mle_CIs$Country2 == "Venezuela")
+mle_CIs$Country1[!ind_EV1] <- "Colombia"
+mle_CIs$Country2[!ind_EV2] <- "Colombia"
+mle_CIs$State1[ind_EV1 | mle_CIs$State1 == "Colombia"] <- NA
+mle_CIs$State2[ind_EV2 | mle_CIs$State2 == "Colombia"] <- NA
+unique(c(mle_CIs$Country1, mle_CIs$Country2)) # Check countries
+unique(c(mle_CIs$State1, mle_CIs$State2)) # Check states
+
 # Add cities
 # Check NA returned for missing sids: 
 is.na(metadata['agsdhfsdhf', 'City'])
 mle_CIs$City1 = metadata[mle_CIs$individual1, 'City']
 mle_CIs$City2 = metadata[mle_CIs$individual2, 'City']
-mle_CIs$State1 = metadata[mle_CIs$individual1, 'STATE']
-mle_CIs$State2 = metadata[mle_CIs$individual2, 'STATE']
 
 # Add city comparisons
 mle_CIs$City12 = apply(mle_CIs[,c('City1','City2')], 1, function(x)paste(sort(x),collapse="_"))
 
-# Add city distance and access
+# Add city distance 
 city_comb <- unique(mle_CIs[, c("City1", "City2", "City12")])
 intercity_dists <- sapply(1:nrow(city_comb), function(i){
   gcd.hf(A = as.matrix(LonLats[city_comb[i, "City1"], c('Longitude', 'Latitude')]), 
@@ -40,9 +52,9 @@ intercity_dists <- sapply(1:nrow(city_comb), function(i){
 names(intercity_dists) <- city_comb$City12
 mle_CIs$geo_dist <- intercity_dists[mle_CIs$City12]
 
-# Access
-mle_CIs$Coast <- LonLats[mle_CIs$City1, "CoastAccess"] == 1 & LonLats[mle_CIs$City2, "CoastAccess"] == 1
-mle_CIs$Port <- LonLats[mle_CIs$City1, "Port"] == 1 & LonLats[mle_CIs$City2, "Port"] == 1
+# Add InterCoast and InterPort
+mle_CIs$InterCoast <- (mle_CIs$City1 != mle_CIs$City2) & (LonLats[mle_CIs$City1, "CoastAccess"] == 1) & (LonLats[mle_CIs$City2, "CoastAccess"] == 1) 
+mle_CIs$InterPort <- (mle_CIs$City1 != mle_CIs$City2) & (LonLats[mle_CIs$City1, "Port"] == 1) & (LonLats[mle_CIs$City2, "Port"] == 1)  
 
 # Check NA returned for missing sids: yes
 metadata['agsdhfsdhf', 'COLLECTION.DATE']
@@ -61,7 +73,7 @@ mle_CIs$year2 = metadata[mle_CIs$individual2, 'Year']
 mle_CIs$time_dist = abs(difftime(as.Date(mle_CIs$date1), 
                                  as.Date(mle_CIs$date2), units = 'weeks'))
 
-# Add year dist based on yearly label (e.g. not round to less than, more than a year)
+# Add year dist based on yearly label (not rounded to nearest year)
 mle_CIs$year_diff <- abs(as.numeric(mle_CIs$year1) - as.numeric(mle_CIs$year2))
 
 # Add sample comp

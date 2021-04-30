f29ab192eae00a87c1355877faf5ffc19102ec39
artaylor85 <aimee.r.taylor.85@gmail.com>
2021-04-30 18:38:06 +0200

    Add alternative analysis

--- Code/Extended_analysis/Generate_regression_trends.R
+++ Code/Extended_analysis/Generate_regression_trends.R
@@ -43,6 +43,19 @@
 #' Being across coastal cities increases the log odds of being highly related 
 #' A unit increase in year diff decreases the log odds of being highly related 
 #' A unit increase in geo dist decreases the log odds of being highly related 
+#' 
+#' Alternative approach: Regress estimated per-inter-location-pair probabilities
+#' of high-relatedness onto per-location-pair variables (geo_dist, international
+#' etc.). This is similar to regressing per-inter-location-pair high-relatedness
+#' fractions onto per-location-pair variables, but it adjusts for differences in
+#' sampling collection dates. I do this by first fitting a logistic regression
+#' to generate per-inter-location-pair effects, which are then regressed onto
+#' per-inter-location-pair variables. The final model I arrive at suggests
+#' comparisons with Venezuela drive trends in geo_dist, while InterCoast
+#' comparisons are significantly more related. However, it's important to note
+#' that this 2-step approach is far from ideal. For example, it does not account
+#' for uncertainty in the estimated per-location-pair effects, which are based
+#' on very different sample counts and shares
 ##############################################################################
 rm(list = ls())
 require(dplyr) # For data wrangling
@@ -250,7 +263,7 @@ for(j in 1:length(fullmodel_coef)){
   ind <- !is.na(coefficient_formulas[,to_plot,"Est"])
   plot(x = coefficient_formulas[,to_plot,"Est"], 
        y = 1:length(formulas), 
-       pch = c(16,1)[(coefficient_formulas[,to_plot,"Prob"] > 0.05)+1], 
+       pch = c(16,1)[(coefficient_formulas[,to_plot,"Prob"] >= 0.05)+1], 
        xlab = to_plot, ylab = "", yaxt = "n", bty = "n", 
        panel.first = abline(h = which(ind), col = "gray"))
   axis(side = 2, at = 1:length(formulas), labels = formulas, cex.axis = 0.35, las = 1)
@@ -306,10 +319,11 @@ outliers <- finalmodel.augmented[which(abs(finalmodel.augmented$.std.resid) >= 3
 finalmodel_rminf <- glm(finalformula, family = binomial(link = "logit"), 
     data = mle_CIs[which(abs(finalmodel.augmented$.std.resid) < 3),])
 finalmodel_rmven <- glm(finalformula, family = binomial(link = "logit"), 
-                        data = mle_CIs[mle_CIs$geo_dist < 1000,])
+                        data = mle_CIs[mle_CIs$geo_dist >= 0 & mle_CIs$geo_dist < 1000,])
 summary(finalmodel)$coefficients 
 summary(finalmodel_rminf)$coefficients # same interpretation
-summary(finalmodel_rmven)$coefficients # same interpretation
+summary(glm(finalformula, family = binomial(link = "logit"), 
+            data = mle_CIs[mle_CIs$geo_dist < 1000,]))
 
 # Residual plots (not ideal) -------------------------------
 qqnorm(y = finalmodel.augmented$.resid)
@@ -320,6 +334,186 @@ X2<-sum(residuals(finalmodel, type="pearson")^2) # Pearson statistic
 phi<-X2/finalmodel$df.residual # estimate of dispersion parameter using the Pearson statistic
 phi<-finalmodel$deviance/finalmodel$df.residual # estimate of dispersion parameter using the residual deviance
 # phi < 1 therefore data are not overdispersed
+
+
+
+#============================================================================
+# Alternative approach: 
+#============================================================================
+#' Regress year_diff adjusted per-inter-location-pair probabilities of high
+#' relatedness onto per-location-pair variables (geo_dist, international etc.).
+#' This is similar to regressing per-location-pair highly relatedness fractions
+#' onto per-location-pair variables, but adjusts for differences in year_diff. I
+#' do this by first fitting a logistic regression of high-relatedness onto
+#' year_diff and per-location-pair dummy variables. This provides
+#' per-location-pair effects, which are then regressed onto
+#' per-inter-location-pair variables. The final model I arrive at suggests
+#' comparisons with Venezuela drive trends in geo_dist, while InterCoast
+#' comparisons are significantly more related. However, it's important to note
+#' that this 2-step approach is far from ideal. For example, it does not account
+#' for uncertainty in the estimated per-location-pair effects, which are based
+#' on very different sample counts and shares
+
+# For plotting 
+pchs = 22:24
+bgs = RColorBrewer::brewer.pal(3,"Dark2")
+cols = c("white", "black")
+
+# Make dummy variable columns from City12 --------------------------------------------
+length(unique(mle_CIs$City12)) # should add 89 columns 
+mle_CIs_wide <- mle_CIs %>%
+  tidyr::pivot_wider(names_from = City12, values_from = City12)
+ncol(mle_CIs_wide) - ncol(mle_CIs) # Added 88 columns, why not 89? Removes City12
+colnames(mle_CIs)[!colnames(mle_CIs) %in% colnames(mle_CIs_wide)]
+
+# Make dummy variable 0/1 -----------------------------------------------------------
+mle_CIs_wide[,unique(mle_CIs$City12)][!is.na(mle_CIs_wide[,unique(mle_CIs$City12)])] <- "1"
+mle_CIs_wide[,unique(mle_CIs$City12)][is.na(mle_CIs_wide[,unique(mle_CIs$City12)])] <- "0"
+
+# Create City12 matrix with metadata ------------------------------------------------
+City12_matrix <- mle_CIs %>% 
+  group_by(City12) %>% 
+  summarise(fract = mean(highly_related), 
+            geo_dist = mean(geo_dist),
+            International = unique(International), 
+            Ecuador = unique(Ecuador), 
+            InterMarine = unique(InterMarine), 
+            InterCoast = unique(InterCoast), 
+            InterPort = unique(InterPort),
+            n = n()) %>% 
+  arrange(fract) 
+
+
+City12_coef_names <- City12_matrix$City12[City12_matrix$n >= 10] 
+
+# Logistic regression with year_diff and City12 predictors, -------------------------
+# excluding location pairs with too few samples (otherwise GLM doesn't converge). 
+# Takes a while
+GLM <- glm(sprintf("highly_related ~ year_diff + %s", paste(City12_coef_names, collapse = " + ")), 
+           family = binomial(link = "logit"), data = mle_CIs_wide)
+summary(GLM)
+
+# Add City12 coefficients to the City12_matrix ---------------------------------------
+City12_matrix$Coef <- NA # Initialise column
+City12_matrix$Coef[City12_matrix$n >= 10] <- GLM$coefficients[paste0(City12_coef_names,1)]
+
+
+# Plot time-adjusted probabilities ---------------------------------------------------
+# Temporal effect is small, a.s. logit(Coef) and fract are highly correlated. 
+# NB some fract = 0 or 1 so lost in linear visualisation, i.e. y = coef, x = logit(fract)
+# All the coefficients (could add errir bars)
+par(mar = c(4,5,1,1))
+plot(y = arm::invlogit(City12_matrix$Coef), 
+     x = City12_matrix$fract,
+     panel.first = grid(), bty = "n", 
+     ylab = "Estimated per-location-pair probability of high relatedness \n post adjustment for differences in sampling collection dates", 
+     xlab = "Observed per-location-pair fraction of high relatedness", 
+     pch = pchs[as.numeric(City12_matrix$Ecuador)],
+     bg = bgs[as.numeric(City12_matrix$InterMarine)], 
+     col = cols[as.numeric(City12_matrix$International)+1])
+legend("top", cex = 0.75, bty = "n", inset = c(0.05,0.15), 
+       pch = pchs, pt.bg = "white", title = "Shape",
+       legend  = c("Extra Ecuador", "Inter Ecuador", "Intra Ecuador"))
+legend("center", cex = 0.75, bty = "n", inset = c(0.05,0), 
+       pch = 22, pt.bg = "gray", col = cols, title = "Outline colour",
+       legend  = c("Domestic", "International"))
+legend("bottom", cex = 0.75, bty = "n", inset = c(0.05,0.15), 
+       pch = 22, pt.bg = bgs, title = "Fill colour", 
+       legend  = c("Not coastal", "Coastal", "Port"))
+
+# Sanity check of change in rank for intra-location fractions to probabilities
+# Probabilities > fractions (unsurprising since probabilities are for year_diff = 1)
+City12_matrix[City12_matrix$geo_dist == 0, c("City12", "fract", "Coef")] %>% arrange(Coef)
+plot(y = c(arm::invlogit(City12_matrix$fract[City12_matrix$geo_dist == 0]), 
+           arm::invlogit(City12_matrix$Coef[City12_matrix$geo_dist == 0])), 
+     x = jitter(rep(0:1, each = sum(City12_matrix$geo_dist == 0)), factor = 0.5), 
+     ylim = c(0.5,1), xlim = c(-0.5, 1.5), 
+     xlab = "", xaxt = "n", bty = "n", 
+     ylab = "Observed fraction value / Estimated probability value", 
+     col = 1:sum(City12_matrix$geo_dist == 0))
+# arrows(x0 = rep(0.15, sum(City12_matrix$geo_dist == 0)), 
+#        x1 = rep(0.85, sum(City12_matrix$geo_dist == 0)), 
+#        y0 = arm::invlogit(City12_matrix$fract[City12_matrix$geo_dist == 0]), 
+#        y1 = arm::invlogit(City12_matrix$Coef[City12_matrix$geo_dist == 0]),
+#        length = 0.05, lwd = 0.5)
+axis(side = 1, at = 0:1, labels = c("Observed \n fraction", "Estimated \n probability"), line = 1, 
+     tcl = NA)
+
+# Linear regression of coefficients --------------------------------------
+Full_LM <- lm("Coef ~ geo_dist + International + InterCoast + InterPort + Ecuador", 
+              data = City12_matrix[City12_matrix$geo_dist >= 0,])
+summary(Full_LM); anova(Full_LM)
+
+Red_LM <- lm("Coef ~ geo_dist + InterCoast", 
+             data = City12_matrix[City12_matrix$geo_dist >= 0,])
+summary(Red_LM); Red_LM_augmented <- broom::augment(Red_LM)
+
+# Plot fitted model and estimated probabilities --------------------------
+plot(y = City12_matrix$Coef[City12_matrix$geo_dist >= 0], 
+     x = City12_matrix$geo_dist[City12_matrix$geo_dist >= 0], 
+     ylab = "Estimated per-location-pair logit probability of high relatedness \n post adjustment for differences in sampling collection dates",
+     xlab = "Great circle distance (km)", 
+     pch = pchs[as.numeric(City12_matrix$Ecuador[City12_matrix$geo_dist >= 0])],
+     bg = bgs[as.numeric(City12_matrix$InterMarine[City12_matrix$geo_dist >= 0])], 
+     col = cols[as.numeric(City12_matrix$International[City12_matrix$geo_dist >= 0])+1])
+
+Red_LM_augmented <- Red_LM_augmented %>% arrange(InterCoast, geo_dist)
+lines(Red_LM_augmented$.fitted[!Red_LM_augmented$InterCoast], 
+      x = Red_LM_augmented$geo_dist[!Red_LM_augmented$InterCoast], 
+      pch = 20, col = bgs[1])
+lines(Red_LM_augmented$.fitted[Red_LM_augmented$InterCoast], 
+      x = Red_LM_augmented$geo_dist[Red_LM_augmented$InterCoast], 
+      pch = 20, col = bgs[2])
+legend("topright", cex = 0.75, bty = "n", inset = c(0.05,0.30), 
+       pch = pchs, pt.bg = "white", title = "Shape",
+       legend  = c("Extra Ecuador", "Inter Ecuador", "Intra Ecuador"))
+legend("topright", cex = 0.75, bty = "n", inset = c(0.05,0), 
+       pch = 22, pt.bg = "gray", col = cols, title = "Outline colour",
+       legend  = c("Domestic", "International"))
+legend("topright", cex = 0.75, bty = "n", inset = c(0.05,0.15), 
+       pch = 22, pt.bg = bgs, title = "Fill colour", 
+       legend  = c("Not coastal", "Coastal", "Port"))
+
+
+# Clearly, comparisons with Venezuela are driving the trend in above plot -----
+# Check result without: geo_dist effect is now significantly positive!
+Final_LM <- lm("Coef ~ geo_dist + InterCoast", 
+               data = City12_matrix[City12_matrix$geo_dist >= 0 & City12_matrix$geo_dist < 1000,])
+summary(Final_LM); #plot(Final_LM);  
+Final_LM_augmented <- broom::augment(Final_LM)
+
+# Plot fitted model and estimated probabilities --------------------------
+plot(y = City12_matrix$Coef[City12_matrix$geo_dist >= 0 & City12_matrix$geo_dist < 1000], 
+     x = City12_matrix$geo_dist[City12_matrix$geo_dist >= 0 & City12_matrix$geo_dist < 1000], 
+     ylab = "Estimated per-location-pair logit probability of high relatedness \n post adjustment for differences in sampling collection dates",
+     xlab = "Great circle distance (km)", 
+     pch = pchs[as.numeric(City12_matrix$Ecuador[City12_matrix$geo_dist >= 0 & City12_matrix$geo_dist < 1000])],
+     bg = bgs[as.numeric(City12_matrix$InterMarine[City12_matrix$geo_dist >= 0 & City12_matrix$geo_dist < 1000])], 
+     col = cols[as.numeric(City12_matrix$International[City12_matrix$geo_dist >= 0 & City12_matrix$geo_dist < 1000])+1])
+
+Final_LM_augmented <- Final_LM_augmented %>% arrange(InterCoast, geo_dist)
+lines(Final_LM_augmented$.fitted[!Final_LM_augmented$InterCoast], 
+      x = Final_LM_augmented$geo_dist[!Final_LM_augmented$InterCoast], 
+      pch = 20, col = bgs[1])
+lines(Final_LM_augmented$.fitted[Final_LM_augmented$InterCoast], 
+      x = Final_LM_augmented$geo_dist[Final_LM_augmented$InterCoast], 
+      pch = 20, col = bgs[2])
+legend("bottomleft", cex = 0.75, bty = "n", inset = c(0.05,0.30), 
+       pch = pchs, pt.bg = "white", title = "Shape",
+       legend  = c("Extra Ecuador", "Inter Ecuador", "Intra Ecuador"))
+legend("bottomleft", cex = 0.75, bty = "n", inset = c(0.05,0), 
+       pch = 22, pt.bg = "gray", col = cols, title = "Outline colour",
+       legend  = c("Domestic", "International"))
+legend("bottomleft", cex = 0.75, bty = "n", inset = c(0.05,0.15), 
+       pch = 22, pt.bg = bgs, title = "Fill colour", 
+       legend  = c("Not coastal", "Coastal", "Port"))
+
+
+# Model summaries
+summary(GLM)
+summary(Full_LM)
+summary(Red_LM)
+summary(Final_LM) 
 
 
 
